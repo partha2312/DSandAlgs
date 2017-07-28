92d3f4a85cb7ceff3147c813ded4fe56e61186ec
Parthasarathy Krishnamurthy <parthasarathy2312@gmail.com>
2017-07-27 21:11:39 -0500

    Trees

---
+++ DataStructures/Trees/tree_orders.java
@@ -0,0 +1,110 @@
+import java.util.*;
+import java.io.*;
+
+public class tree_orders {
+    class FastScanner {
+		StringTokenizer tok = new StringTokenizer("");
+		BufferedReader in;
+
+		FastScanner() {
+			in = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() throws IOException {
+			while (!tok.hasMoreElements())
+				tok = new StringTokenizer(in.readLine());
+			return tok.nextToken();
+		}
+	
+		int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+	}
+
+	public class TreeOrders {
+		int n;
+		int[] key, left, right;
+		ArrayList<Integer> result;
+		
+		void read() throws IOException {
+			FastScanner in = new FastScanner();
+			n = in.nextInt();
+			key = new int[n];
+			left = new int[n];
+			right = new int[n];
+			for (int i = 0; i < n; i++) { 
+				key[i] = in.nextInt();
+				left[i] = in.nextInt();
+				right[i] = in.nextInt();
+			}
+		}
+
+		void inOrderHelper(int index){
+			if(left[index] != -1) inOrderHelper(left[index]);
+			result.add(key[index]);
+			if(right[index] != -1) inOrderHelper(right[index]);
+		}
+
+		void preOrderHelper(int index){
+			result.add(key[index]);
+			if(left[index] != -1) preOrderHelper(left[index]);
+			if(right[index] != -1) preOrderHelper(right[index]);
+		}
+
+		void postOrderHelper(int index){
+			if(left[index] != -1) postOrderHelper(left[index]);
+			if(right[index] != -1) postOrderHelper(right[index]);
+			result.add(key[index]);
+		}
+
+		List<Integer> inOrder() {
+			result = new ArrayList<Integer>();
+                        // Finish the implementation
+                        // You may need to add a new recursive method to do that
+			inOrderHelper(0);
+			return result;
+		}
+
+		List<Integer> preOrder() {
+			result = new ArrayList<Integer>();
+                        // Finish the implementation
+                        // You may need to add a new recursive method to do that
+			preOrderHelper(0);
+			return result;
+		}
+
+		List<Integer> postOrder() {
+			result = new ArrayList<Integer>();
+                        // Finish the implementation
+                        // You may need to add a new recursive method to do that
+			postOrderHelper(0);
+			return result;
+		}
+	}
+
+	static public void main(String[] args) throws IOException {
+            new Thread(null, new Runnable() {
+                    public void run() {
+                        try {
+                            new tree_orders().run();
+                        } catch (IOException e) {
+                        }
+                    }
+                }, "1", 1 << 26).start();
+	}
+
+	public void print(List<Integer> x) {
+		for (Integer a : x) {
+			System.out.print(a + " ");
+		}
+		System.out.println();
+	}
+
+	public void run() throws IOException {
+		TreeOrders tree = new TreeOrders();
+		tree.read();
+		print(tree.inOrder());
+		print(tree.preOrder());
+		print(tree.postOrder());
+	}
+}
