95f3b1c37fddf1606dd676b6c501b906ad194732
Parthasarathy Krishnamurthy <parthasarathy2312@gmail.com>
2017-08-13 16:12:08 -0500

    Knuth Morris Pratt

---
+++ String Algorithms/AdvancedStringAlgs/KnuthMorrisPratt.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class KnuthMorrisPratt {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public int[] computePrefix(String string){
+        int[] prefixes = new int[string.length()];
+        prefixes[0] = 0;
+        int border = 0;
+        for(int i=1;i<string.length();++i){
+            while(border > 0 && string.charAt(i) != string.charAt(border)){
+                border = prefixes[border-1];
+            }
+            if(string.charAt(i) == string.charAt(border)){
+                border = border + 1;
+            } else {
+                border = 0;
+            }
+            prefixes[i] = border;
+        }
+        return prefixes;
+    }
+
+    // Find all the occurrences of the pattern in the text and return
+    // a list of all positions in the text (starting from 0) where
+    // the pattern starts in the text.
+    public List<Integer> findPattern(String pattern, String text) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        // Implement this function yourself
+        String newString = pattern + "$" + text;
+        int[] prefixes = computePrefix(newString);
+        for(int i=(pattern.length()+1);i < newString.length();i++){
+            if(prefixes[i] == pattern.length()){
+                result.add(i - (2*pattern.length()));
+            }
+        }
+        return result;
+    }
+
+    static public void main(String[] args) throws IOException {
+        new KnuthMorrisPratt().run();
+    }
+
+    public void print(List<Integer> x) {
+        for (int a : x) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+
+    public void run() throws IOException {
+        FastScanner scanner = new FastScanner();
+        String pattern = scanner.next();
+        String text = scanner.next();
+        List<Integer> positions = findPattern(pattern, text);
+        print(positions);
+    }
+}
